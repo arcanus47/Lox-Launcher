c612e87ef6cf80f159c2e41873d0783a6fbd95be
arcanus47 <126821851+arcanus47@users.noreply.github.com>
2024-08-05 11:48:36 -0400

    Update Lox Launcher.py

--- Lox Launcher.py
+++ Lox Launcher.py
@@ -2,7 +2,19 @@ import minecraft_launcher_lib
 import subprocess
 import sys
 import os
+import json
 from pathlib import Path
+
+""" Ruta de los archivos de Minecraft linux
+if os.name == "nt":
+    minecraft_directorio = str(os.getenv("APPDATA")+".lox_launcher")
+elif os.name == "posix":
+    minecraft_directorio = str(Path.home())+("/.lox_launcher")
+"""
+
+# Ruta de los archivos de Minecraft
+usuario_windows = os.environ["USERNAME"]
+minecraft_directorio = f"C:/Users/{usuario_windows}/AppData/Roaming/.lox_launcher"
 
 # Barra de Progreso
 current_max = 0
@@ -18,63 +30,82 @@ def set_max(new_max: int):
     global current_max
     current_max = new_max
 
-minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
-
 callback = {
     "setStatus": set_status,
     "setProgress": set_progress,
     "setMax": set_max
 }
 
-# Ruta de los archivos de Minecraft
-if os.name == "nt":
-    minecraft_directorio = str(os.getenv("APPDATA")+".lox_launcher")
-elif os.name == "posix":
-    minecraft_directorio = str(Path.home())+("/.lox_launcher")
+# Función para guardar el usuario y la cantidad de RAM
+def guardar_usuario_y_ram(usuario_minecraft, ram):
+    datos = {
+        "usuario": usuario_minecraft,
+        "ram": ram
+    }
+    ruta_archivo_json = "datos_usuario_ram.json"
+    with open(ruta_archivo_json, "w") as archivo_json:
+        json.dump(datos, archivo_json, indent=4)
+    
+    print(f"Datos guardados en {ruta_archivo_json}")
 
-# Instalación de Minecraft
-def install_minecraft():
+# Función para cargar el usuario y la cantidad de RAM
+def cargar_usuario_y_ram():
+    ruta_archivo_json = "datos_usuario_ram.json"
+    if os.path.exists(ruta_archivo_json):
+        with open(ruta_archivo_json, "r") as archivo_json:
+            datos = json.load(archivo_json)
+            return datos.get("usuario", ""), datos.get("ram", "")
+    return "", ""
+
+# Función para instalar Minecraft
+def instalar_minecraft():
     minecraft_version = input("Versión de Minecraft: ")
-    minecraft = minecraft_launcher_lib.install.install_minecraft_version(
-        minecraft_version, minecraft_directorio, callback=callback)
+    minecraft_launcher_lib.install.install_minecraft_version(minecraft_version, minecraft_directorio, callback=callback)
     print(f"Versión instalada: {minecraft_version}")
 
-# Instalación de Forge
-def install_forge():
+# Función para instalar Forge
+def instalar_forge():
     forge_version = input("Versión de Forge: ")
     forge = minecraft_launcher_lib.forge.find_forge_version(forge_version)
-    print(forge)
-    minecraft_launcher_lib.forge.install_forge_version(
-        forge, minecraft_directorio, callback=callback)
+    minecraft_launcher_lib.forge.install_forge_version(forge, minecraft_directorio, callback=callback)
     print(f"Versión instalada: {forge_version}")
 
-# Instalación de Fabric
-def install_fabric():
-    fabric_version = input("Versión de Fabric: ")
-    fabric = minecraft_launcher_lib.fabric.install_fabric(fabric_version, minecraft_directorio, callback=callback)
-    print(f"Versión instalada: {fabric_version}")
+# Función para instalar Fabric
+def instalar_fabric():
+    version_fabric = input("Versión de Fabric: ")
+    
+    try:
+        # Obtener la última versión del loader de Fabric para la versión especificada
+        loader_version = minecraft_launcher_lib.fabric.get_latest_loader_version()
+        
+        # Instalar Fabric con la versión del loader obtenida
+        minecraft_launcher_lib.fabric.install_fabric(version_fabric, loader_version, minecraft_directorio, callback=callback)
+        print(f"Fabric versión {version_fabric} instalado correctamente.")
+        
+    except Exception as e:
+        print(f"Error: {e}")
 
-# Versiones de Minecraft instaladas
-def ejecuta_mine(mine_user):
-    forts = minecraft_launcher_lib.utils.get_installed_versions(
-        minecraft_directorio)
-    for fort in forts:
-        print(fort["id"])
-    version = input("Versión: ")
+# Función para ejecutar Minecraft
+def ejecutar_minecraft(usuario_minecraft, ram):
+    versions = minecraft_launcher_lib.utils.get_installed_versions(minecraft_directorio)
+    for version in versions:
+        print(version["id"])
+    version = input("Versión de Minecraft: ")
 
-    options = {
-        "username": mine_user,
+    opciones = {
+        "username": usuario_minecraft,
         "uuid": "",
         "token": "",
-
-        "jvmArguments": ["-Xmx1024M", "-Xms1024M"],  # The jvmArguments
-        "launcherVersion": "1.1",
+        "jvmArguments": [f"-Xmx{ram}G", f"-Xms{ram}G"],
+        "version_launcher": "1.2"
     }
 
     # Ejecutar Minecraft
-    minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(
-        version, minecraft_directorio, options)
+    minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version, minecraft_directorio, opciones)
     subprocess.run(minecraft_command)
+    
+    # Guardar el usuario y la cantidad de RAM
+    guardar_usuario_y_ram(usuario_minecraft, ram)
 
 # Shell del programa
 def menu():
@@ -89,50 +120,60 @@ def menu():
     print("------------------------------------------------------------------")
     print("------------------------------------------------------------------")
     print("\n")
-    
-    mine_user = input("Digite su nombre de usuario: ")
+    print("--- Menú de Opciones ---")
+    print("1. Instalar Minecraft")
+    print("2. Instalar Forge")
+    print("3. Instalar Fabric")
+    print("4. Ejecutar Minecraft")
+    print("5. Acerca de")
+    print("6. Salir")
     print("\n")
-    
-    print("Opciones \n",
-          "1- Instalar Minecraft \n", 
-          "2- Instalar Forge \n",
-          "3- Instalar Fabric \n",
-          "4- Ejecutar Minecraft \n", 
-          "5- Acerca de \n", 
-          "6- Salir \n")
-    
-    formulario = input(f"{mine_user} selecciona una opción: ")
-    if formulario == "1":
-        install_minecraft()
+
+# Cargar usuario y RAM guardados
+usuario_predeterminado, ram_predeterminado = cargar_usuario_y_ram()
+
+# Menú de opciones
+while True:
+    menu()
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        instalar_minecraft()
         
-    if formulario == "2":
-        install_forge()
+    elif opcion == "2":
+        instalar_forge()
         
-    if formulario == "3":
-        install_fabric()
+    elif opcion == "3":
+        instalar_fabric()
         
-    if formulario == "4":
-        ejecuta_mine(mine_user)
+    elif opcion == "4":
+        # Solicitar inputs del usuario con valores predeterminados
+        usuario_minecraft = input(f"Nombre de usuario [{usuario_predeterminado}]: ") or usuario_predeterminado
+        ram = input(f"Cantidad de RAM (en GB) [{ram_predeterminado}]: ") or ram_predeterminado
+        ejecutar_minecraft(usuario_minecraft, ram)
         
-    if formulario == "5":
-        print("LOX LAUNCHER: \n",
-              "1.1 \n",
-              " \n",
-              "DIRECTORIO: \n",
-              minecraft_directorio, "\n",
-              " \n",
-              "CREADOR: \n",
+    elif opcion == "5":
+        print("CREADOR: \n",
               "Cheremi Checo Dominguez \n",
+              " \n",
+              "NOMBRE DEL PROGRAMA: \n",
+              "Lox Launcher \n",
+              " \n",
+              "VERSIÓN: \n",
+              "1.2 \n",
+              " \n",
+              "RUTA DE LOS ARCHIVOS: \n",
+              minecraft_directorio, "\n",
               " \n",
               "COLABORADORES: \n",
               "Anderson Guzman Abreu \n",
               " \n",
               "LICENCIA: \n",
               "MIT - https://github.com/arcanus47/Lox-Launcher/blob/main/LICENSE \n")
-        print(input())
-    
-    if formulario == "6":
-        exit()
-
-if __name__ == "__main__":
-    menu()
+        input("Presione Enter para continuar...")
+        
+    elif opcion == "6":
+        print("Saliendo del programa...")
+        break
+    else:
+        print("Opción no válida. Por favor, seleccione una opción del 1 al 6.")
